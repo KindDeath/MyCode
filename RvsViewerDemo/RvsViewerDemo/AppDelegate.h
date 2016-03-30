d7d4da63301029f63ba86bfc6fd19129ae93078b
GYJ <1157756119@qq.com>
2016-03-30 10:21:37 +0800

    creat new project

---
+++ RvsViewerDemo/RvsViewerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RvsViewerDemo
+//
+//  Created by GYJ on 16/3/30.
+//  Copyright © 2016年 GYJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

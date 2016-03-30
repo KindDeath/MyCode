93c53565633e6ca4db2a576dbe126f8fab658cc2
GYJ <1157756119@qq.com>
2016-03-30 10:53:05 +0800

    delete storyboard add pch

--- RvsViewerDemo/RvsViewerDemo/Classes/ViewController/YJLoginVC.h
+++ RvsViewerDemo/RvsViewerDemo/Classes/ViewController/YJLoginVC.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  YJLoginVC.h
 //  RvsViewerDemo
 //
 //  Created by GYJ on 16/3/30.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface YJLoginVC : UIViewController
 
 @end
-
---
+++ RvsViewerDemo/RvsViewerDemo/Classes/ViewController/YJLoginVC.m
@@ -0,0 +1,26 @@
+//
+//  YJLoginVC.m
+//  RvsViewerDemo
+//
+//  Created by GYJ on 16/3/30.
+//  Copyright © 2016年 GYJ. All rights reserved.
+//
+
+#import "YJLoginVC.h"
+
+@interface YJLoginVC ()
+
+@end
+
+@implementation YJLoginVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"登录";
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+
+@end
